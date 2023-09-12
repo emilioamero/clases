5578d1dd94d259b62a9ede49a863ca23aa8a9d8d
emilioamero <emilio.amero@gmil.com>
2023-09-12 19:25:59 -0300

    Clase 12092023

---
+++ Clases Programacion 1/4_Arrays/codigo.js
@@ -0,0 +1,56 @@
+let valor = 10;
+//document.write(valor);
+
+//let frutas= ["Banana", "Cereza", "Tomate", "Manzana", "Pera"];
+/* 
+let edades= [1, 4, 6, 7,8];
+let datosramdom= [1, 4, "Emilio", 7 ,8.0,valor ];
+
+document.write(frutas + "</br>");
+
+frutas.push("Manzana Verde");
+
+document.write(frutas); */
+
+let arr = [];
+
+//let agregarFruta = prompt("Ingrese una Fruta");
+/* 
+ arr= ["Banana", "Cereza", "Tomate", "Manzana", "Pera"];
+ arr.push(agregarFruta); // Agrega elemento a la array
+ document.write(arr  + "</br>"); // br es un salto de linea
+
+
+ arr.pop();
+
+ document.write(arr  + "</br>"); */
+
+//document.write("<h1>Emilio</h1>")
+arr = ["Banana", "Cereza", "Tomate", "Manzana", "Pera"];
+document.write(arr + "</br>");
+
+arr.pop(); //Quitar el ultimo elemento
+
+arr.pop(); //Quitar el ultimo elemento
+
+arr.pop(); //Quitar el ultimo elemento
+
+arr.pop(); //Quitar el ultimo elemento
+
+arr.pop(); //Quitar el ultimo elemento
+
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+
+document.write(arr + "</br>");
+document.write(arr.length + "</br>");
+
+//crear un array con una frase, donde cada palabra sea un elemento del array.
+
+/*  let frase =["Hola", "como", "estas"];
+
+document.write(frase) */
