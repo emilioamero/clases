934329817542712ddbdfda8142a076e84fe08ff1
emilioamero <emilio.amero@gmil.com>
2023-10-14 20:36:18 -0300

    crudmysql

---
+++ Clases Programacion 2/CRUD_mySql/enviar/backend/database.js
@@ -0,0 +1,27 @@
+// database.js
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: '192.168.3.10', //localhost si la base esta instalada en su pc, 192.168.3.10 desde el instituto o conexion vpn
+  user: '33309073',
+  password: '-33309073*.*',
+  database: 'lavadero33309073',
+});
+
+// Intenta conectarte a la base de datos
+connection.connect((err) => {
+  if (err) {
+
+    console.error('-----Error al conectar a la base de datos:----------------------------------------------------------');
+    console.error('', err);
+    console.error('----------------------------------------------------------------------------------------------------');
+   // console.error('Error al conectar a la base de datos:', err);
+    // Puedes tomar medidas adicionales aquí, como registrar el error o intentar reconectar.
+  } else {
+    console.log('Conexión exitosa a la base de datos MySQL');
+    // Ahora puedes ejecutar consultas SQL o realizar operaciones en la base de datos
+  }
+});
+
+
+module.exports = connection.promise();
