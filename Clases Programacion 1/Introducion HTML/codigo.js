fa0dc578d0506113722710d2e82f9cb90ea0d18d
emilioamero <emilio.amero@gmil.com>
2023-10-16 22:54:52 -0300

    html primer anio

---
+++ Clases Programacion 1/Introducion HTML/codigo.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let titulo = document.getElementById("titulo");
+
+console.log(titulo)
+
+titulo.style.color = "blue";
+titulo.style.background = "red";
+titulo.innerText="hola";
+
+function test(){
+    titulo.style.color = "green";
+}  */
